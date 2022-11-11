0eb0fa9e549da6663e218394c6b51e520e36181d
moclan110 <vulan11101997@gmail.com>
2022-11-11 08:28:16 +0700

    luyen tap cau truc dieu kien 01

---
+++ main.js
@@ -0,0 +1,68 @@
+// Bài 1
+// let a = +prompt('Enter number a');
+// let b = +prompt('Enter number b');
+// if(a % b == 0) {
+//     alert('a chia hết cho b');
+// } else {
+//     alert('a không chia hết cho b');
+// }
+
+// Bài 2
+// let age = +prompt('Enter age of student');
+// if(age < 15){
+//     document.write('Học sinh không đủ điều kiện vào học lớp 10');
+// }
+
+// Bài 3
+// let number = +prompt('Enter a number');
+// if(number > 0) {
+//     document.write('Số ' + number + ' lớn hơn 0');
+// } else if(number < 0) {
+//     document.write('Số ' + number + ' nhỏ hơn 0');
+// }
+
+// Bài 4
+// let a = +prompt('enter number a');
+// let b = +prompt('enter number b');
+// let c = +prompt('enter number c');
+// let max = a;
+// if(b>max) {
+//     max=b;
+// }
+// if(c>max) {
+//     max=c;
+// }
+// document.write(max + ' là số lớn nhất');
+
+// Bài 5
+// let score = +prompt('Enter score');
+// if(score >= 75) {
+//     document.write('Loại A');
+// } else if(score >= 60) {
+//     document.write('Loại B');
+// } else if(score >= 45) {
+//     document.write('Loại C');
+// } else if(score >= 35) {
+//     document.write('Loại D');
+// } else {
+//     document.write('Loại E');
+// }
+
+// Bài 6
+// 5% nếu tổng doanh số nhỏ hơn hoặc bằng 100 triệu.
+// 10% nếu tổng doanh số nhỏ hơn hoặc bằng 300 triệu.
+// 20 % nếu tổng doanh số là lớn hơn 300 triệu.
+
+// let turnover = +prompt('enter turnover');
+// let commission;
+//
+// if(turnover <= 100) {
+//     commission = 5/100*turnover;
+// } else if(turnover <= 300) {
+//     commission = 10/100*turnover;
+// } else {
+//     commission = 20/100*turnover;
+// }
+// document.write('Hoa hồng nhận được là '+ commission + ' triệu');
+
+
